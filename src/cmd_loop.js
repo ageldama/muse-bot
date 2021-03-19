22170b776dc1ddaf8c7d10c6031a809e0e68f23b
jonghyouk yun <ageldama@gmail.com>
2021-03-19 20:54:37 +0900

    --wip-- [skip ci]

--- src/cmd_loop.js
+++ src/cmd_loop.js
@@ -1,36 +1,34 @@
 'use strict'
 
+const { withTimeout, Mutex } = require('async-mutex')
 const { parseCmd } = require('./cmd_parser')
 const { routeCmd } = require('./cmd_handler')
 const { resolveOrCatch } = require('./promise_utils')
 
-function startChatCmdLoop(intervalSecs, telegramClient, cmdHandler) {
-  let running = false
-  return setInterval(() => {
-    if (running) {
-      // 중복해서 실행중인 커맨드를 또 fetch하지 않도록.
-      return
+function procUpdate(cmdHandler, telegramClient, update) {
+  resolveOrCatch(
+    routeCmd(parseCmd(update.message.text), cmdHandler, telegramClient),
+    (err) => {
+      console.log('ERR', err)
     }
-    running = true
-    //
-    telegramClient
-      .getUpdates()
+  )
+}
+
+function startChatCmdLoop(intervalSecs, telegramClient, cmdHandler) {
+  const mutex = withTimeout(new Mutex(), 10 * 1000)
+  return setInterval(() => {
+    mutex
+      .runExclusive(() => telegramClient.getUpdates())
       .then((updates) => {
-        updates.forEach((update) => {
-          resolveOrCatch(
-            routeCmd(parseCmd(update.message.text), cmdHandler, telegramClient),
-            (err) => {
-              console.log('ERR', err)
-              // resolveOrCatch(telegramClient.sendMessage(`ERR: ${err}`))
-            }
-          )
-        })
+        updates.forEach((update) =>
+          procUpdate(cmdHandler, telegramClient, update)
+        )
+        // returns nothing
       })
       .catch((err) => {
         console.log('ERR', err)
         // resolveOrCatch(telegramClient.sendMessage(`ERR: ${err}`))
       })
-      .finally(() => (running = false))
   }, intervalSecs * 1000)
 }
 
--- src/ytdl_worker.js
+++ src/ytdl_worker.js
@@ -1,4 +1,5 @@
 'use strict'
+const { Mutex } = require('async-mutex')
 
 class YtdlWorker {
   constructor(dbYtdlQueue, processManager, builtinsDir) {
@@ -9,49 +10,43 @@ class YtdlWorker {
     this.proc = null
   }
 
+  _spawnYtdl(ytdlEntry) {
+    this.proc = this.processManager.spawn(
+      this.builtinsDir + '/ytdl.sh',
+      [ytdlEntry.url],
+      // onStdout
+      (data) => console.log('[ytdl:STDOUT] ' + data),
+      // onStderr
+      (data) => console.log('[ytdl:STDERR] ' + data),
+      // onExitCode
+      (pid, entry, code) => {
+        console.log(
+          `[ytdl:EXITED] with ${code}, PID:${pid}, ${entry.prog}, ${entry.argv}`
+        )
+        //
+        const idx = this.processManager.findIndexOfSpawnedProcessByPid(pid)
+        if (idx >= 0) this.processManager.deleteSpawnedProcessEntry(idx)
+      }
+    )
+    return this.proc
+  }
+
   start(seconds = 10) {
-    let running = false
+    const mutex = new Mutex()
     this.intervalId = setInterval(() => {
-      if (running) {
-        return
-      }
-      running = true
-      // 다른 프로세스 실행 중?
-      if (this.proc && !this.proc.exited) {
-        running = false
-        return
-      }
-      //
-      this.dbYtdlQueue
-        .getOldest()
-        .then((ytdlEntry) => {
+      mutex.acquire().then(() => {
+        // 다른 프로세스 실행 중?
+        if (this.proc && !this.proc.exited) {
+          return
+        }
+        //
+        return this.dbYtdlQueue.getOldest().then((ytdlEntry) => {
           if (ytdlEntry) {
-            // spawn ytdl
-            this.proc = this.processManager.spawn(
-              this.builtinsDir + '/ytdl.sh',
-              [ytdlEntry.url],
-              // onStdout
-              (data) => console.log('[ytdl:STDOUT] ' + data),
-              // onStderr
-              (data) => console.log('[ytdl:STDERR] ' + data),
-              // onExitCode
-              (pid, entry, code) => {
-                console.log(
-                  `[ytdl:EXITED] with ${code}, PID:${pid}, ${entry.prog}, ${entry.argv}`
-                )
-                //
-                const idx = this.processManager.findIndexOfSpawnedProcessByPid(
-                  pid
-                )
-                if (idx >= 0) this.processManager.deleteSpawnedProcessEntry(idx)
-              }
-            )
-            //
+            this._spawnYtdl(ytdlEntry)
             return this.dbYtdlQueue.delete(ytdlEntry.id)
           }
         })
-        .then(() => {})
-        .finally(() => (running = false))
+      })
     }, seconds * 1000)
   }
 
